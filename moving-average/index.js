d739c0246c8528c0c303dc5ae678c98ce5561277
TeunSpithoven <teun.spithoven@gmail.com>
2024-05-30 13:12:13 +0200

    moving average calculation

--- moving-average/index.js
+++ moving-average/index.js
@@ -34,8 +34,7 @@ ctx.stroke();
 var sineCanvasContainer = document.getElementById("sine-container");
 sineCanvasContainer.appendChild(sineCanvas);
 
-// Create a new sine wave with noise
-
+// Create a sine wave with noise
 var sineWaveWithNoise = [];
 for (var i = 0; i < sampleRate * seconds; i++) {
   const randomAmplitude = volume - Math.floor(Math.random() * 10) / 30;
@@ -45,14 +44,14 @@ for (var i = 0; i < sampleRate * seconds; i++) {
 var sineCanvasWithNoise = document.createElement("canvas");
 var ctx = sineCanvasWithNoise.getContext("2d");
 
-const canvasWithNoiseWidth = sineWave.length;
+const canvasWithNoiseWidth = sineWaveWithNoise.length;
 const canvasWithNoiseHeight = 200;
 sineCanvasWithNoise.width = canvasWidth;
 sineCanvasWithNoise.height = canvasHeight;
 
 ctx.beginPath();
 ctx.moveTo(0, 100);
-for (let i = 0; i < sineWave.length; i++) {
+for (let i = 0; i < sineWaveWithNoise.length; i++) {
   ctx.lineTo(i, canvasHeight / 2 - sineWaveWithNoise[i] * 100);
 }
 
@@ -64,4 +63,51 @@ var sineCanvasWithNoiseContainer = document.getElementById(
 sineCanvasWithNoiseContainer.appendChild(sineCanvasWithNoise);
 
 // Do a moving average calculation on the sine wave with noise.
+const sampleSize = 6;
+
+var sineWaveAverage = [];
+for (let i = 0; i < sineWaveWithNoise.length; i++) {
+  var sum = 0;
+  var average = 0;
+
+  if (i >= sampleSize / 2) {
+    // ook terug kijken
+    for (
+      let addSample = 0 - sampleSize / 2;
+      addSample < sampleSize / 2;
+      addSample++
+    ) {
+      sum += sineWaveWithNoise[i + addSample];
+    }
+  } else {
+    // alleen vooruit kijken
+    for (let addSample = 0; addSample < sampleSize; addSample++) {
+      sum += sineWaveWithNoise[i + addSample];
+    }
+  }
+  average = sum / sampleSize;
+
+  sineWaveAverage.push(average);
+}
+
 // Display the result in the sine-moving-container section.
+var sineCanvasAverage = document.createElement("canvas");
+var ctx = sineCanvasAverage.getContext("2d");
+
+const canvasWithNoiseAverageWidth = sineWaveAverage.length;
+const canvasWithNoiseAverageHeight = 200;
+sineCanvasAverage.width = canvasWidth;
+sineCanvasAverage.height = canvasHeight;
+
+ctx.beginPath();
+ctx.moveTo(0, 100);
+for (let i = 0; i < sineWaveAverage.length; i++) {
+  ctx.lineTo(i, canvasHeight / 2 - sineWaveAverage[i] * 100);
+}
+
+ctx.stroke();
+
+var sineCanvasAverageContainer = document.getElementById(
+  "sine-average-container"
+);
+sineCanvasAverageContainer.appendChild(sineCanvasAverage);
